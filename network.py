53323e5f6cfa911070741d0ebb79a2a9acac8c15
CheeChizzle <ifebichigozirim@sewanhakaschools.org>
2021-08-19 17:09:02 -0400

    ignore path files

--- network.py
+++ network.py
@@ -77,13 +77,32 @@ class SingleViewResNet(nn.Module):
         temporary_model = torchvision.models.resnet50(pretrained=True)
         layers = list(temporary_model.children())
         layers = layers[:-1]
-        shape = layers[-1].shape[1]
         self.net = nn.Sequential(
             *layers,
-            nn.Linear(shape, 6)
+            nn.Linear(512, 6)
         )
 
     def forward(self, x):
         return self.net(x)
 
-# class MultiViewResNet(nn.Module):
+class MultiViewResNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = SingleViewResNet()
+    
+    def forward(self,images):
+        # images.shape == B V C H W
+        first_view = images[:,0,...] # B C H W
+        second_view = images[:,1,...]
+        third_view = images[:,2,...]
+        fourth_view = images[:,3,...]
+
+        first_view_score = self.net(first_view)
+        second_view_score = self.net(second_view)
+        third_view_score = self.net(third_view)
+        fourth_view_score = self.net(fourth_view)
+
+        score = first_view_score + second_view_score + third_view_score + fourth_view_score
+
+        # scores.shape  == B K
+        return score
